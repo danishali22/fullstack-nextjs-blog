f15aa26dfae2e039b8b3015e0aea60c5baf65fc0
Danish Ali <dali27363@gmail.com>
2025-02-17 15:39:05 +0500

    Add layout components and theme provider; implement navbar with search and toggle mode

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/home/header/navbar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
