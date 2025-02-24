9356664263ef47cfac86214d329d847637c8e690
Danish Ali <dali27363@gmail.com>
2025-02-24 11:16:36 +0500

    Add TopArticles and BlogFooter components; update HeroSection and Home page layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,9 @@
+import { BlogFooter } from "@/components/home/BlogFooter";
 import Navbar from "@/components/home/header/navbar";
 import HeroSection from "@/components/home/hero-section";
+import TopArticles from "@/components/home/top-articles";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
@@ -7,6 +11,21 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroSection />
+      <section className="relative py-16 md:py-24">
+        <div className="conatainer mx-auto px-4">
+            <div className="mb-12 text-center">
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-xl">Featured Articles</h2>
+                <p>Discover our popular and trending content</p>
+            </div>
+        </div>
+        <TopArticles />
+        <div>
+            <Link href={"/articles"}>
+                <Button className="rounded-full hover:bg-gray-900 hover:text-white dark:bg-white dark:hover:text-gray-900"></Button>
+            </Link>
+        </div>
+      </section>
+      <BlogFooter />
     </div>
   );
 };
--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <div className="absolute inset-0 before:absolute before:left-1/4 before:top-0 before:h-[500px] before:w-[500px] before:rounded-full before:bg-gradient-to-r before:from-violet-600/20 before:to-indigo-600/20 before:blur-3xl" />
       <div className="container relative mx-auto flex h-full flex-col items-center justify-center px-4 py-24 md:flex-row md:py-32">
         <div className="flex-1 space-y-8 text-center md:text-left">
-          <h1 className="text-4xl font-bold tracking-light text-white sm:text-5xl md:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
             Explore the World Through
             <span className="bg-gradient-to-r from-violet-400 bg-clip-text text-transparent">
               {" "}
