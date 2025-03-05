c4d919000934de639232b681fe662570298d07be
Danish Ali <dali27363@gmail.com>
2025-03-05 14:18:57 +0500

    Implement responsive left sidebar with navigation links and toggle functionality

--- components/dashboard/left-sidebar.tsx
+++ components/dashboard/left-sidebar.tsx
@@ -1,25 +1,35 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import {
   Sheet,
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from '../ui/button';
-import { LayoutDashboard } from 'lucide-react';
+import { BarChart, FileText, LayoutDashboard, MessageCircle, Settings } from 'lucide-react';
+import Link from 'next/link';
 
 
 const LeftSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Sheet>
-      <SheetTrigger>
-        <Button>
-          <LayoutDashboard className='w-5 h-5' />
-        </Button>
-      </SheetTrigger>
-      <SheetContent>
+    <div>
+      <Sheet open={isOpen} onOpenChange={() => setIsOpen(false)}>
+        <SheetTrigger>
+          <Button className='md:hidden m-4'>
+            <LayoutDashboard className="w-5 h-5" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={'left'} className='w-[250px]'>
+          <DashboardSidebar />
+        </SheetContent>
+      </Sheet>
+
+      <div className='hidden md:block h-screen w-[250px] border-r bg-background'>
         <DashboardSidebar />
-      </SheetContent>
-    </Sheet>
+      </div>
+    </div>
   );
 }
 
@@ -28,7 +38,43 @@ export default LeftSidebar
 const DashboardSidebar = () => {
   return (
     <div className='w-full px-4 py-6'>
-
+      <div className='flex items-center gap-2 mb-8 px-2'>
+        <Link href={'/'}>
+          <span className='text-xl font-bold'>ByteCode</span>
+        </Link>
+      </div>
+      <nav>
+        <Link href="/dashboard">
+          <Button variant={"ghost"} className='w-full justify-start'>
+            <LayoutDashboard className='w-5 h-5' />
+            Overview
+          </Button>
+        </Link>
+        <Link href="/articles">
+          <Button variant={"ghost"} className='w-full justify-start'>
+            <FileText className='w-5 h-5' />
+            Articles
+          </Button>
+        </Link>
+        <Link href="/comments">
+          <Button variant={"ghost"} className='w-full justify-start'>
+            <MessageCircle className='w-5 h-5' />
+            Comments
+          </Button>
+        </Link>
+        <Link href="/analytics">
+          <Button variant={"ghost"} className='w-full justify-start'>
+            <BarChart className='w-5 h-5' />
+            Analytics
+          </Button>
+        </Link>
+        <Link href="/settings">
+          <Button variant={"ghost"} className='w-full justify-start'>
+            <Settings className='w-5 h-5' />
+            Settings
+          </Button>
+        </Link>
+      </nav>
     </div>
   )
 }
