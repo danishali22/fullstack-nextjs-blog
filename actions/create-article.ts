cc024e7a2e001e096d0188abf4ef2b917be3cd82
Danish Ali <dali27363@gmail.com>
2025-03-07 15:35:10 +0500

    Add Cloudinary integration for article image uploads; enhance article creation form with error handling and loading state

--- actions/create-article.ts
+++ actions/create-article.ts
@@ -3,6 +3,15 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import {z} from "zod";
+import {v2 as cloudinary, UploadApiResponse} from "cloudinary";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const createArticleSchema = z.object({
     title: z.string().min(3).max(100),
@@ -16,11 +25,11 @@ type CreateArticlesFormState = {
         category?: string[],
         featuredImage?: string[],
         content?: string[],
-        forrmErrors?: string[],
+        formErrors?: string[],
     }
 }
 
-export const createArticle = async (formData: FormData): Promise<CreateArticlesFormState> => {
+export const createArticle = async (prevState: CreateArticlesFormState, formData: FormData): Promise<CreateArticlesFormState> => {
     const result = createArticleSchema.safeParse({
         title: formData.get("title"),
         category: formData.get("category"),
@@ -37,13 +46,89 @@ export const createArticle = async (formData: FormData): Promise<CreateArticlesF
     if(!userId) {
         return {
             errors: {
-                forrmErrors: ["You have to login first"]
+                formErrors: ["You have to login first"]
+            }
+        }
+    }
+
+    const existingUser = await prisma.user.findUnique({
+        where: {clerkUserId: userId}
+    });
+    if (!existingUser){
+        return {
+            errors: {
+                formErrors: ["User not found, Please register before creating an article"]
             }
         }
     }
 
     // start creating article
+    const imageFile = formData.get("featuredImage") as File | null;
+    if(!imageFile || imageFile.name == "undefined") {
+        return {
+            errors: {
+                featuredImage: ["Image file is required"]
+            }
+        }
+    }
 
+    const arrayBuffer = await imageFile.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult: UploadApiResponse | undefined = await new Promise(
+        (resolve, reject) => {
+            const uploadStream = cloudinary.uploader.upload_stream(
+                { resource_type: "auto" },
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                }
+            );
+            uploadStream.end(buffer);
+        }
+    );
+
+    const imageUrl = uploadResult?.secure_url;
+
+    if (!imageUrl) {
+        return {
+            errors: {
+                featuredImage: ["Failed to upload image. Please try again."],
+            },
+        };
+    }
+
+    try {
+        await prisma.articles.create({
+            data: {
+                title: result.data.title,
+                category: result.data.category,
+                content: result.data.content,
+                featuredImage: imageUrly,
+                authorId: existingUser.id
+            }
+        });
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                errors: {
+                    formErrors: [error.message],
+                },
+            };
+        } else {
+            return {
+                errors: {
+                    formErrors: ["Some internal server error occurred."],
+                },
+            };
+        }
+
+    }
+
+    revalidatePath("/dashboard")
     redirect("/dashboard")
 
 }
